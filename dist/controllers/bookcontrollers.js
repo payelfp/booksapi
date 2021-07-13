e8eae39c917ca249a98dd80522ebcc099fc3a178
payelfp <payel.rakshit@firstprinciples.io>
2021-07-13 08:52:47 +0530

    commit

---
+++ dist/controllers/bookcontrollers.js
@@ -0,0 +1,64 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.addBook = exports.updateBook = exports.deleteBook = exports.getBook = exports.allBooks = void 0;
+const book_1 = __importDefault(require("./../book"));
+let allBooks = (req, res) => {
+    let books = book_1.default.find((err, books) => {
+        if (err) {
+            res.send("Error!");
+        }
+        else {
+            res.send(books);
+        }
+    });
+};
+exports.allBooks = allBooks;
+let getBook = (req, res) => {
+    let book = book_1.default.findById(req.params.id, (err, book) => {
+        if (err) {
+            res.send(err);
+        }
+        else {
+            res.send(book);
+        }
+    });
+};
+exports.getBook = getBook;
+let deleteBook = (req, res) => {
+    let book = book_1.default.deleteOne({ _id: req.params.id }, (err) => {
+        if (err) {
+            res.send(err);
+        }
+        else {
+            res.send("Successfully Deleted Book");
+        }
+    });
+};
+exports.deleteBook = deleteBook;
+let updateBook = (req, res) => {
+    console.log(req.body);
+    let book = book_1.default.findByIdAndUpdate(req.params.id, req.body, (err, book) => {
+        if (err) {
+            res.send(err);
+        }
+        else {
+            res.send("Successfully updated book!");
+        }
+    });
+};
+exports.updateBook = updateBook;
+let addBook = (req, res) => {
+    var book = new book_1.default(req.body);
+    book.save((err) => {
+        if (err) {
+            res.send(err);
+        }
+        else {
+            res.send(book);
+        }
+    });
+};
+exports.addBook = addBook;
---
+++ src/app.ts
@@ -0,0 +1,21 @@
+import express, { Request, Response } from "express";
+
+import * as bookController from "./controllers/bookcontrollers";
+
+// Our Express APP config
+const app = express();
+app.set("port", process.env.PORT || 3000);
+
+// API Endpoints
+app.get("/", (req: Request, res: Response) => res.send("hi"))
+
+// API Endpoints
+app.get("/books", bookController.allBooks);
+app.get("/book/:id", bookController.getBook);
+app.post("/book", bookController.addBook);
+app.put("/book/:id", bookController.updateBook);
+app.delete("/book/:id", bookController.deleteBook);
+
+const server = app.listen(app.get("port"), () => {
+  console.log("App is running on http://localhost:%d", app.get("port"));
+});
